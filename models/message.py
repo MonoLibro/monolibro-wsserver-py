e3691da16447aaa72af00faba387cdc4108cfe0c
mclt0568 <tis7bfrankie@gmail.com>
2021-11-12 22:16:01 +0800

    predefined fields in message model

--- models/message.py
+++ models/message.py
@@ -1,4 +1,9 @@
 class Message:
+    version : int = None
+    sessionID : str = None
+    details : dict = None
+    operation: str = None
+    data : dict = None
     def __init__(self, object):
         required_fields = ["version", "sessionID", "details", "operation", "data"]
         for i in required_fields:
