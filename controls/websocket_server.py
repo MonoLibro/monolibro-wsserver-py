b67579d67fe00e687fd758240d715c5efe13070f
mclt0568 <tis7bfrankie@gmail.com>
2021-11-12 20:56:09 +0800

    decode message on handler

--- controls/websocket_server.py
+++ controls/websocket_server.py
@@ -1,5 +1,7 @@
 import websockets
 import asyncio
+import base64
+import json
 
 class WebSocketServer:
     def __init__(self, ip, port):
@@ -9,10 +11,15 @@ class WebSocketServer:
     
     def on_message(self):
         async def raw_handler(ws, path):
-            pass
+            while True:
+                raw_messages = (await ws.recv()).split(".")
+                decoded = [base64.b64decode(i + '=' * (4 - len(i) % 4)).decode() for i in raw_messages]
+                signature = decoded[0]
+                message = json.loads(decoded[1])
         return raw_handler
 
     async def start(self):
+        print(f"listening on ws://{self.ip}:{self.port}")
         async with websockets.serve(self.on_message(), self.ip, self.port):
             await asyncio.Future()
 
