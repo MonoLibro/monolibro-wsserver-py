51ffccf68edaea6a566b8bfbcf32cb04bbcb84a1
mclt0568 <tis7bfrankie@gmail.com>
2021-11-12 19:49:34 +0800

    fix typos in class

--- controls/websocket_server.py
+++ controls/websocket_server.py
@@ -13,11 +13,11 @@ class WebSocketServer:
         return raw_handler
 
     async def start(self):
-        async with websockets.serve(self.on_message(), "127.0.0.1", 3200):
+        async with websockets.serve(self.on_message(), self.ip, self.port):
             await asyncio.Future()
 
     def handler(self, name):
         def wrapper(func):
-            self.hanlders[name] = func
+            self.handlers[name] = func
             return func
         return wrapper
