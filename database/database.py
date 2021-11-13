729e8b3aba47e8c014b05eabc2ace0459dbef261
mclt0568 <tis7bfrankie@gmail.com>
2021-11-13 22:36:16 +0800

    implemented database

---
+++ database/database.py
@@ -0,0 +1,64 @@
+import sqlite3
+from . import Table
+
+
+class Database:
+    def __init__(self, database_name):
+        self.name = database_name
+        self.db = sqlite3.connect(database_name)
+        self.schemas = {
+            "Users": [
+                ["userID", "char(32)", "PRIMARY KEY", "NOT NULL"],
+                ["firstName", "char(256)", "NOT NULL"],
+                ["lastName", "char(256)", "NOT NULL"],
+                ["publicKey", "text", "NOT NULL"]
+            ]
+        }
+
+    def __getitem__(self, table_name):
+        if not (table_name in self.get_table_names()):
+            return None
+        table = Table(self, table_name, self.schemas[table_name])
+        return table
+
+    def execute_only(self, statement):
+        self.db.cursor().execute(statement)
+
+    def execute(self, statement):
+        cur = self.db.cursor()
+        return cur.execute(statement)
+    
+    def has_table(self, table_name):
+        sql = f'select * from sqlite_schema where name = "{table_name}" and type = "table";'
+        for i in self.execute(sql):
+            return True
+        return False
+    
+    def get_table_names(self):
+        sql = 'select name from sqlite_schema where type = "table";'
+        return [i[0] for i in self.execute(sql)]
+
+    def drop_table(self, table_name):
+        self.execute_only(f'drop table {table_name}')
+
+    def force_reset(self):
+        tables = self.get_table_names()
+        for table in tables:
+            self.drop_table(table)
+        for table in self.schemas.keys():
+            fields = self.schemas[table]
+            self.execute_only(f"create table {table} ({','.join([' '.join(i) for i in fields])})")
+            print(f"create table {table} ({','.join([' '.join(i) for i in fields])})")
+
+    def commit(self):
+        self.db.commit()
+
+    def close(self):
+        self.db.close()
+
+    def reconnect(self):
+        try:
+            self.db.close()
+        except Exception:
+            pass
+        self.db = sqlite3.connect(self.name)
---
+++ database/table.py
@@ -0,0 +1,45 @@
+class Table:
+    def __init__(self, db, table_name, schema):
+        self.db = db
+        self.table_name = table_name
+        self.schema = schema
+
+    def get_primary_key(self):
+        keys = []
+        for field in self.schema:
+            if "PRIMARY KEY" in field[2:]:
+                keys.append(field[0])
+        return keys
+
+    def __getitem__(self, keys):
+        try:
+            if (type(keys) == dict):
+                conditions = []
+                for column in keys.keys():
+                    value = keys[column]
+                    conditions.append(f"{column} = '{value}'")
+                return self.db.execute(f"select * from {self.table_name} where {' and '.join(conditions)};")
+            else:
+                primary_key = self.get_primary_key()[0]
+                return self.db.execute(f'select * from {self.table_name} where {primary_key} = "{keys}"')
+        except Exception:
+            return []
+
+    def __delitem__(self, keys):
+        try:
+            if (type(keys) == dict):
+                conditions = []
+                for column in keys.keys():
+                    value = keys[column]
+                    conditions.append(f"{column} = '{value}'")
+                return self.db.execute(f"delete from {self.table_name} where {' and '.join(conditions)};")
+            else:
+                primary_key = self.get_primary_key()[0]
+                return self.db.execute(f'delete from {self.table_name} where {primary_key} = "{keys}"')
+        except Exception:
+            pass
+
+    def insert(self, values):
+        values = [f"'{i}'" for i in values]
+        values_str = ", ".join(values)
+        self.db.execute(f"insert into {self.table_name} values ({values_str})")
