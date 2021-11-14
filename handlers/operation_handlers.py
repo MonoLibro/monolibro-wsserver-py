348538149038abf1021a0b38982bd38c146658a7
mclt0568 <tis7bfrankie@gmail.com>
2021-11-14 22:09:53 +0800

    join_network operation adds the user to a list

--- handlers/operation_handlers.py
+++ handlers/operation_handlers.py
@@ -1,5 +1,6 @@
 from loguru import logger
 
+from monolibro.models.user import User
 from monolibro.models import Operation
 
 
@@ -7,8 +8,17 @@ def register(handler):
     logger.info("Registering operations")
 
     @handler.handler(Operation.JOIN_NETWORK)
-    def join_network(ws, proxy, payload, signature):
-        print("If you ever see this message being written to stdout, this marks the milestone of human civilization "
-              "development, and a step foward to world peace.")
+    async def join_network(ws, proxy, payload, signature):
+        data = payload.data
+        if "userID" not in data:
+            logger.info("A client trys to join the network but rejected: Insufficient payload data")
+            await ws.close()
+            return
+        user_id = data["userID"]
+        logger.info(f"A client has joined the network with user of {user_id}")
+        if user_id not in proxy.users:
+            proxy.users[user_id] = User(id=user_id)
+        proxy.users[user_id].clients.append(ws)
+
 
     logger.info("Operations registered")
