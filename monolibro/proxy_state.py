f13e7d86cb12d377f12a58dcf72b4bd85fa0993f
mclt0568 <tis7bfrankie@gmail.com>
2021-11-18 22:15:59 +0800

    added voting session to be part of proxy_state

--- monolibro/proxy_state.py
+++ monolibro/proxy_state.py
@@ -1,6 +1,7 @@
 from .models import User
-
+from .voting_session import VotingSession
 
 class ProxyState:
     def __init__(self) -> None:
         self.users: dict[str, User] = {}
+        self.votes: dict[str, VotingSession] = {}
