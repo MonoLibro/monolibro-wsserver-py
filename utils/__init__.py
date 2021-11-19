79aff831493e2fa751f6bb22591cba0e91b4c186
Leung Yau Ming <leungyauming2005@gmail.com>
2021-11-19 19:24:49 +0800

    refactor: rewrite `message` utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,7 @@
 from . import base64
 from . import config
 from . import fs
+from . import message
 from . import pem
 from . import rsa
 from . import cli
--- utils/message.py
+++ utils/message.py
@@ -2,49 +2,32 @@ import json
 
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
-from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey, RSAPublicKey
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
+from pydantic import ValidationError
 
 from monolibro.models import Payload
 from utils import base64
 
 
-def encode(payload: Payload, private_key: RSAPrivateKey):
-    payload_encoded = base64.encode_url_no_padding(payload.json().encode())
-    signature = private_key.sign(
-        payload_encoded.encode(),
-        padding.PSS(
-            padding.MGF1(hashes.SHA256()),
-            padding.PSS.MAX_LENGTH
-        ),
-        hashes.SHA256()
-    )
-    signature_encoded = base64.encode_url_no_padding(signature)
-
-    return f"{payload_encoded}.{signature_encoded}"
-
-
-class MalformedMessageError(Exception):
+class DeserializePayloadError(Exception):
     pass
 
 
-def decode(raw_msg: str, public_key: RSAPublicKey) -> (Payload, bytes):
-    raw_msg_slices = raw_msg.split(".")
-    if len(raw_msg_slices) != 2:
-        raise MalformedMessageError("more than one message parts are found")
+def deserialize(message: str, private_key: RSAPrivateKey) -> Payload:
+    payload_encrypted = base64.decode_url_no_padding(message)
 
-    payload_encoded = raw_msg_slices[0]
-    signature_encoded = raw_msg_slices[1]
-
-    signature = base64.decode_url_no_padding(signature_encoded)
-    public_key.verify(
-        signature, payload_encoded.encode(),
-        padding.PSS(
+    payload_json = private_key.decrypt(
+        payload_encrypted,
+        padding.OAEP(
             padding.MGF1(hashes.SHA256()),
-            padding.PSS.MAX_LENGTH
-        ),
-        hashes.SHA256()
+            hashes.SHA256(),
+            None
+        )
     )
 
-    payload = Payload(**json.loads(base64.decode_url_no_padding(payload_encoded)))
+    try:
+        payload = Payload(**json.loads(payload_json))
+    except ValidationError:
+        raise DeserializePayloadError("missing information in payload")
 
-    return payload, signature
+    return payload
