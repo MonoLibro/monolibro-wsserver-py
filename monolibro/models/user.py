f36988b75572291080ab2bf6d5b3c1cd0807f566
Leung Yau Ming <leungyauming2005@gmail.com>
2021-11-21 10:15:29 +0800

    make `User` a plain class

--- monolibro/models/user.py
+++ monolibro/models/user.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel
 from websockets.legacy.server import WebSocketServerProtocol
 
 
-class User(BaseModel):
-    id: str
-    clients: list[WebSocketServerProtocol] = []
+class User:
+    def __init__(self, user_id: str, clients: list[WebSocketServerProtocol] = None):
+        self.id = user_id
+        self.client = clients if clients else []
