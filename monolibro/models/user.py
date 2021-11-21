bdda9a8c8a1a93bce964d9e44cf6d381fb9df08a
Leung Yau Ming <leungyauming2005@gmail.com>
2021-11-21 09:59:58 +0800

    add typing to `User.clients`

--- monolibro/models/user.py
+++ monolibro/models/user.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
+from websockets.legacy.server import WebSocketServerProtocol
 
 
 class User(BaseModel):
     id: str
-    clients: list = []
+    clients: list[WebSocketServerProtocol] = []
