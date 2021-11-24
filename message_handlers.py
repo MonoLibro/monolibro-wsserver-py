690460d15ec53b15c1c7eadf55f9507dffd12732
Leung Yau Ming <leungyauming2005@gmail.com>
2021-11-24 21:09:48 +0800

    fix: resolve unexpected arguments and the missing `clients` attribute

--- message_handlers.py
+++ message_handlers.py
@@ -40,7 +40,7 @@ def register_to_proxy(proxy):
 
         user_id = data.userID
         if user_id not in ctx.state.users:
-            ctx.state.users[user_id] = User(id=user_id)
+            ctx.state.users[user_id] = User(user_id=user_id)
         if ctx.ws not in ctx.state.users[user_id].clients:
             ctx.state.users[user_id].clients.append(ctx.ws)
             logger.info(f"A client of {user_id} has joined the network.")
--- monolibro/models/user.py
+++ monolibro/models/user.py
@@ -4,4 +4,4 @@ from websockets.legacy.server import WebSocketServerProtocol
 class User:
     def __init__(self, user_id: str, clients: list[WebSocketServerProtocol] = None):
         self.id = user_id
-        self.client = clients if clients else []
+        self.clients = clients if clients else []
